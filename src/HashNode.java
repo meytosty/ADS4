a0a0babf2820fec4e0fe686538397f6a9a7b42c5
meytosty <ketebay.b@gmail.com>
2023-05-16 00:17:12 +0600

    complement HashNode class

--- src/HashNode.java
+++ src/HashNode.java
@@ -1,2 +1,12 @@
 public class HashNode<K, V> {
+    private K key;
+    private V value;
+    public HashNode(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+    @Override
+    public String toString() {
+        return "{" + key + " " + value + "}";
+    }
 }
