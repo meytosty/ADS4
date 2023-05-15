423539a75c52eeb2798eaa3fe37918eddbd319ce
meytosty <ketebay.b@gmail.com>
2023-05-16 00:31:04 +0600

    adding documentation to HashNode class

--- src/HashNode.java
+++ src/HashNode.java
@@ -1,19 +1,40 @@
 public class HashNode<K, V> {
-    private K key;
-    private V value;
+    private K key; // field to store key
+    private V value; // field to store value
+    /**
+     * HashNode - constructor to set values in fields
+     * @param key - given key to set
+     * @param value - given value to set
+     */
     public HashNode(K key, V value) {
         this.key = key;
         this.value = value;
     }
+    /**
+     * getKey - method that returns key of node
+     * @return K - key type
+     */
     public K getKey() {
         return key;
     }
+    /**
+     * getValue - method that returns value of node
+     * @return V - value type
+     */
     public V getValue() {
         return value;
     }
+    /**
+     * setValue - method that set value in node
+     * @param value - given value to set
+     */
     public void setValue(V value) {
         this.value = value;
     }
+    /**
+     * toString - method that returns node with specific type
+     * @return String
+     */
     @Override
     public String toString() {
         return "{" + key + " " + value + "}";
