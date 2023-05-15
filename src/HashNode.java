ff61ae1a9c78a2516dddc97ec19ce55a83d486a3
meytosty <ketebay.b@gmail.com>
2023-05-16 00:26:17 +0600

    supplement method put and add methods in HashNode class

--- src/HashNode.java
+++ src/HashNode.java
@@ -5,6 +5,15 @@ public class HashNode<K, V> {
         this.key = key;
         this.value = value;
     }
+    public K getKey() {
+        return key;
+    }
+    public V getValue() {
+        return value;
+    }
+    public void setValue(V value) {
+        this.value = value;
+    }
     @Override
     public String toString() {
         return "{" + key + " " + value + "}";
