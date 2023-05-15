fd61fb34a2604e8b03e2c613e790bd46756b4e91
meytosty <ketebay.b@gmail.com>
2023-05-16 00:35:19 +0600

    complete TestingClass class

--- src/TestingClass.java
+++ src/TestingClass.java
@@ -1,3 +1,10 @@
 public class TestingClass {
-
+    private int val;
+    public TestingClass(int val) {
+        this.val = val;
+    }
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(val);
+    }
 }
