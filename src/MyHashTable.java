e7a421b118475c22acc953dec28359394466dfa8
meytosty <ketebay.b@gmail.com>
2023-05-16 00:33:08 +0600

    adding documentation to MyHashTable class

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,21 +1,37 @@
 import java.util.LinkedList;
-
 public class MyHashTable<K, V> {
-    private LinkedList<HashNode<K, V>>[] chainArray;
+    private LinkedList<HashNode<K, V>>[] chainArray; // linked list to store nodes
     private int M = 11;
-    private int size;
+    private int size; // table size
+    /**
+     * MyHashTable - constructor to create hash table
+     */
     public MyHashTable() {
         this.size = 0;
         this.chainArray = new LinkedList[M];
     }
+    /**
+     * MyHashTable - constructor with additional M to change
+     * @param M - given M to change
+     */
     public MyHashTable(int M) {
         this.M = M;
         this.size = 0;
         this.chainArray = new LinkedList[M];
     }
+    /**
+     * hash - method to hashing key
+     * @param key - given key to hash
+     * @return int
+     */
     public int hash(K key) {
         return key.hashCode() % M;
     }
+    /**
+     * put - method puts a value in hash table with key
+     * @param key - given key to put within
+     * @param value - given value to put
+     */
     public void put(K key, V value) {
         int index = hash(key);
         if (chainArray[index] == null) {
@@ -30,6 +46,11 @@ public class MyHashTable<K, V> {
         chainArray[index].add(new HashNode<K, V>(key, value));
         size++;
     }
+    /**
+     * get - method gets a value with key
+     * @param key - given key to get value within
+     * @return V - value type
+     */
     public V get(K key) {
         int index = hash(key);
         if (chainArray[index] == null) {
@@ -42,6 +63,11 @@ public class MyHashTable<K, V> {
         }
         return null;
     }
+    /**
+     * remove - method removes value with key
+     * @param key - given key to remove value within
+     * @return V - value type
+     */
     public V remove(K key) {
         int index = hash(key);
         if (chainArray[index] == null) {
@@ -57,6 +83,11 @@ public class MyHashTable<K, V> {
         size--;
         return null;
     }
+    /**
+     * contains - method checks is hash table contains given value
+     * @param value - given value to check
+     * @return boolean
+     */
     public boolean contains(V value) {
         for (LinkedList<HashNode<K, V>> list : chainArray) {
             for (HashNode<K, V> node : list) {
@@ -67,6 +98,11 @@ public class MyHashTable<K, V> {
         }
         return false;
     }
+    /**
+     * getKey - method returns key of value
+     * @param value - given value to get key
+     * @return K - key type
+     */
     public K getKey(V value) {
         for (LinkedList<HashNode<K, V>> list : chainArray) {
             for (HashNode<K, V> node : list) {
