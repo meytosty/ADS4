43a525008e8376da7dae66efee4cd29c3d179945
meytosty <ketebay.b@gmail.com>
2023-05-16 00:37:12 +0600

    complete Main class with test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-
+        MyHashTable hashTable = new MyHashTable<TestingClass, String>();
+        Random r = new Random();
+        for (int i = 0; i < 10000; i++) {
+            int val = r.nextInt(0, 9999999);
+            hashTable.put(new TestingClass(val), "Student " + val);
+        }
     }
 }
