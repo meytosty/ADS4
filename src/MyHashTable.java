853b0222437b4bbde73785a208c2c1eea07ff64d
meytosty <ketebay.b@gmail.com>
2023-05-16 00:24:23 +0600

    implement fields, constructors and methods

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,2 +1,34 @@
-public class MyHashTable {
+import java.util.LinkedList;
+
+public class MyHashTable<K, V> {
+    private LinkedList<HashNode<K, V>>[] chainArray;
+    private int M = 11;
+    private int size;
+    public MyHashTable() {
+        this.size = 0;
+        this.chainArray = new LinkedList[M];
+    }
+    public MyHashTable(int M) {
+        this.M = M;
+        this.size = 0;
+        this.chainArray = new LinkedList[M];
+    }
+    public int hash(K key) {
+        return 0;
+    }
+    public void put(K key, V value) {
+
+    }
+    public V get(K key) {
+        return null;
+    }
+    public V remove(K key) {
+        return null;
+    }
+    public boolean contains(V value) {
+        return false;
+    }
+    public K getKey(V value) {
+        return null;
+    }
 }
